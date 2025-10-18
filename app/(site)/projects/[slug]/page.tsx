3de2ab806e7da1ed9f56a3b8803c13a7289a2f3c
Francis <hello@francistries.science>
2025-10-18 21:07:20 +0800

    feat(projects): create project detail and listing pages with dynamic routing

---
+++ app/(site)/projects/[slug]/page.tsx
@@ -0,0 +1,214 @@
+import Link from "next/link"
+import Image from "next/image"
+import { Metadata } from "next"
+import { notFound } from "next/navigation"
+import { HouseIcon, SproutIcon } from "lucide-react"
+
+import { getProjectBySlug } from "@/lib/projects/get-project-by-slug"
+import { getAllProjects } from "@/lib/projects/get-all-projects"
+import { getIconComponent } from "@/helper/get-icon-component"
+
+import {
+  Badge,
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+  Button,
+  MarkdownContent,
+  Separator,
+} from "@/components/ui"
+import BackToTop from "@/components/features/back-to-top-button"
+
+export const dynamic = "force-static"
+
+interface PageProps {
+  params: Promise<{
+    slug: string
+  }>
+}
+
+export default async function ProjectPostPage({ params }: PageProps) {
+  const { slug } = await params
+  const project = getProjectBySlug(slug)
+
+  if (!project) {
+    notFound()
+  }
+
+  return (
+    <div className="mx-auto w-full">
+      <div className="mx-auto w-full max-w-3xl">
+        <article>
+          <header>
+            <div className="mb-4 flex flex-col items-center gap-2 lg:flex-row lg:items-center lg:justify-between">
+              <div className="order-2 flex flex-row items-center gap-2 lg:order-1">
+                {project.active && (
+                  <span className="relative flex size-3">
+                    <span className="bg-success absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
+                    <span className="bg-success relative inline-flex size-3 rounded-full"></span>
+                  </span>
+                )}
+                {project.tags.length > 0 && (
+                  <div className="flex flex-wrap gap-2">
+                    {project.tags.map((tag) => (
+                      <Badge key={tag} className="text-xs uppercase" variant="secondary">
+                        {tag}
+                      </Badge>
+                    ))}
+                  </div>
+                )}
+              </div>
+              <div className="order-1 flex flex-row items-center lg:order-2">
+                <Button
+                  variant="link"
+                  size="sm"
+                  className="text-muted-foreground hover:text-primary flex h-auto flex-row items-center gap-1 p-0 text-sm"
+                >
+                  <HouseIcon className="size-3" />
+                  <Link href={"/"}>Home</Link>
+                </Button>
+                <span className="text-muted-foreground">/</span>
+                <Button
+                  variant="link"
+                  size="sm"
+                  className="text-muted-foreground hover:text-primary flex h-auto flex-row items-center gap-1 p-0 text-sm"
+                >
+                  <SproutIcon className="size-3" />
+                  <Link href={"/projects"}>View projects</Link>
+                </Button>
+              </div>
+            </div>
+            {project.banner && (
+              <div className="mb-6">
+                <Image
+                  src={project.banner}
+                  alt={`${project.title} banner`}
+                  width={1200}
+                  height={420}
+                  className="h-42 w-full rounded-sm object-cover object-center"
+                />
+              </div>
+            )}
+            <h1 className="font-georgia text-foreground mb-4 w-full text-5xl font-medium tracking-tight">
+              {project.title}
+            </h1>
+            <div className="text-muted-foreground mb-4 flex flex-wrap items-center gap-2 text-sm">
+              <div className="flex items-center gap-2">
+                {project.authors.map((author, index) => (
+                  <div key={author.name} className="flex items-center gap-2">
+                    {index > 0 && <span className="text-muted-foreground">, </span>}
+                    <Avatar className="size-5">
+                      <AvatarImage src={author.imageUrl} alt={author.name} />
+                      <AvatarFallback className="text-xs">
+                        {author.name.split(" ")[0][0]}
+                      </AvatarFallback>
+                    </Avatar>
+                    <Link
+                      href={author.url}
+                      target="_blank "
+                      className="underline decoration-dashed underline-offset-2"
+                    >
+                      {author.name}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+              <span className="text-muted-foreground">·</span>
+              <span>{project.readingTime} min read</span>
+              <span className="text-muted-foreground">·</span>
+              <div className="flex flex-row items-center gap-1">
+                <span className="text-muted-foreground text-xs">Updated at</span>
+                <time dateTime={project.date}>
+                  {new Date(project.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </time>
+              </div>
+            </div>
+            <span className="text-muted-foreground text-sm leading-tight italic">
+              {project.description}
+            </span>
+          </header>
+          <div className="mt-4">
+            {project.technologies && project.technologies.length > 0 && (
+              <div className="flex flex-wrap gap-2">
+                {project.technologies.map((tech) => {
+                  const IconComponent = getIconComponent(tech.icon)
+                  return (
+                    <Badge key={tech.name} className="text-xs" variant="secondary">
+                      <div className="flex items-center gap-1 uppercase">
+                        {IconComponent && <IconComponent className="h-3 w-3" />}
+                        <span>{tech.name}</span>
+                      </div>
+                    </Badge>
+                  )
+                })}
+              </div>
+            )}
+          </div>
+          <Separator className="mt-4 mb-8" />
+          <MarkdownContent>{project.content}</MarkdownContent>
+          <BackToTop />
+        </article>
+      </div>
+    </div>
+  )
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { slug } = await params
+  const project = getProjectBySlug(slug)
+
+  if (!project) {
+    return {
+      title: "Project Not Found",
+    }
+  }
+
+  const url = `https://francistries.science/projects/${slug}`
+
+  let ogImageUrl = project.ogImage || ""
+
+  const SITE_ORIGIN = "https://francistries.science"
+  if (ogImageUrl && ogImageUrl.startsWith("/")) {
+    ogImageUrl = SITE_ORIGIN + ogImageUrl
+  }
+
+  return {
+    title: `${project.title} - @francistriesscience`,
+    description: project.description,
+    authors: project.authors.map((author) => ({ name: author.name })),
+    openGraph: {
+      title: project.title,
+      description: project.description,
+      type: "article",
+      publishedTime: project.date,
+      authors: project.authors.map((author) => author.name),
+      url: url,
+      images: [
+        {
+          url: ogImageUrl,
+          width: 1200,
+          height: 630,
+          alt: project.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: project.title,
+      description: project.description,
+      images: [ogImageUrl],
+    },
+  }
+}
+
+export async function generateStaticParams() {
+  const projects = getAllProjects()
+
+  return projects.map((project) => ({
+    slug: project.slug,
+  }))
+}
---
+++ app/(site)/projects/page.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+import Link from "next/link"
+import { HouseIcon } from "lucide-react"
+
+import { getAllProjects } from "@/lib/projects/get-all-projects"
+import { ProjectCard } from "@/components/card/project-card"
+
+import { Card, CardContent, Button, RippleBackground } from "@/components/ui"
+
+export const dynamic = "force-static"
+
+export default async function ProjectsPage() {
+  const projects = getAllProjects()
+
+  return (
+    <div className="flex w-full flex-col items-start gap-4">
+      <div className="flex w-full items-end justify-between">
+        <h2 className="text-xl font-medium">Projects</h2>
+        <Button
+          variant="link"
+          size="sm"
+          className="text-muted-foreground hover:text-primary h-auto p-0 text-sm"
+        >
+          <HouseIcon className="size-3" />
+          <Link href={"/"}>Home</Link>
+        </Button>
+      </div>
+      {projects.length > 0 ? (
+        <div className="flex w-full flex-col items-start gap-2">
+          {projects.map((project) => (
+            <ProjectCard key={project.slug} project={project} />
+          ))}
+        </div>
+      ) : (
+        <div className="w-full">
+          <Card className="relative h-36 overflow-hidden border border-dashed bg-transparent p-4">
+            <RippleBackground className="opacity-20" numCircles={5} />
+            <CardContent className="text-muted-foreground relative z-10 flex h-full flex-col justify-center text-center text-sm">
+              Collecting projects...
+            </CardContent>
+          </Card>
+        </div>
+      )}
+    </div>
+  )
+}
