16f14936ec8cb717f43028cc051fcac017c3afc9
Francis <hello@francistries.science>
2026-01-08 23:35:47 +0800

    feat: update HomePage to use LearningTrackSection and include RepositoriesSection

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,19 @@
 import * as React from "react"
 
-import { LearningTrack } from "@/components/pages/home/learning-track"
+import { LearningTrackSection } from "@/components/pages/home/learning-track-section"
 import { NotebooksSection } from "@/components/pages/home/notebooks-section"
 import { ProjectsSection } from "@/components/pages/home/projects-section"
 import { ExperiencesSection } from "@/components/pages/home/experiences-section"
+import { RepositoriesSection } from "@/components/pages/home/repositories-section"
 
 export default function HomePage() {
   return (
     <>
-      <LearningTrack />
+      <LearningTrackSection />
       <ProjectsSection />
       <ExperiencesSection />
       <NotebooksSection />
+      <RepositoriesSection />
     </>
   )
 }
