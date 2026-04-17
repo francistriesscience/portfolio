4a6ce860bb962a2ebe6ddf5444e8f0a7d5862bae
Francis Ignacio <hello@francistries.science>
2026-04-17 18:54:09 +0800

    cleanup: remove unused UI card dialog and icon components

--- components/ui/dialog/dialog-portal.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import * as React from "react"
-import * as DialogPrimitive from "@radix-ui/react-dialog"
-
-export function DialogPortal({ ...props }: React.ComponentProps<typeof DialogPrimitive.Portal>) {
-  return <DialogPrimitive.Portal data-slot="dialog-portal" {...props} />
-}
