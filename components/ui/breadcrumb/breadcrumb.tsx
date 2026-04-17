06a846e1a0c13ec2e3d94c0fc9ed8ba39ca2de93
Francis Ignacio <hello@francistries.science>
2026-04-17 18:54:21 +0800

    cleanup: remove legacy UI components

--- components/ui/breadcrumb/breadcrumb.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-
-export function Breadcrumb({ ...props }: React.ComponentProps<"nav">) {
-  return <nav aria-label="breadcrumb" data-slot="breadcrumb" {...props} />
-}
