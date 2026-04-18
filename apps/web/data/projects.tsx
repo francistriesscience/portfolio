8767b1367fbea97e012c26bd555846a80642389d
Francis Ignacio <hello@francistries.science>
2026-04-18 23:18:30 +0800

    feat(projects): add initial project data with details and images

--- apps/web/data/projects.tsx
+++ apps/web/data/projects.tsx
@@ -2,6 +2,31 @@ export interface Project {
   name: string
   url: string
   description?: string
+  image: string
+  stack: string[]
+  colab: boolean
 }
 
-export const PROJECTS: Project[] = []
+export const PROJECTS: Project[] = [
+  {
+    name: "Chromiq",
+    url: "https://chromiq.mnemora.org",
+    image: "https://i.imgur.com/mozUPte.gif",
+    stack: ["Next.js", "Flask"],
+    colab: false,
+  },
+  {
+    name: "Seismic Signature Analysis for Tsunami",
+    url: "https://colab.research.google.com/drive/17rKFbYb_F5gqIyFtYP8f8Ejnkdm-YRsW?usp=sharing",
+    image: "https://i.imgur.com/CtAE79v.gif",
+    stack: ["Python", "Scikit-learn", "XGBoost"],
+    colab: true,
+  },
+  {
+    name: "Predicting Calorie Expenditure",
+    url: "https://colab.research.google.com/drive/1VB8UHgkbE989lxI_j9FIOAdTtK_zFF6K?usp=drive_link",
+    image: "https://i.imgur.com/CeVn61E.gif",
+    stack: ["Python", "Scikit-learn"],
+    colab: true,
+  },
+]
